0d15ad7ccb30b94d3644460092a96683181c0e51
Jorge Garzón <150094617+jorgeGrVl@users.noreply.github.com>
2023-11-28 14:34:08 -0600

    Final

--- Guerrero.cpp
+++ Guerrero.cpp
@@ -3,21 +3,28 @@
 
 using namespace std;
 
+//Constructor predeterminado de la clase Guerrero. Crea objetos con valores default.
 Guerrero::Guerrero(){
 }
 
+//Constructor parametrizado de la clase Guerrero. Crea objetos con valores definidos.
+//Utiliza el método constructor parametrizado de la clase Personaje para inicializar atributos.
 Guerrero::Guerrero(string _nombre, int _vida, int _fuerza, int _defensa, int _velocidad, Habilidad _habilidad, Herramienta _herramienta)
         :Personaje(_nombre, _vida, _fuerza, _defensa, _velocidad, _habilidad, _herramienta){
 }
 
+//Regresa la clase del Guerrero.
 string Guerrero::getClase(){
     return clase;
 }
 
-void Guerrero::recibeFuria(){
+//Incrementa la fuerza del Guerrero si su vida e menor o igual a 10. Regresa un mensaje descriptivo.
+string Guerrero::recibeFuria(){
+    string mensaje = "";
     if(getVida() <= 10){
         setFuerza(getFuerza() + furia);
-    cout << endl <<"- " << getNombre() << " activo la habilidad Furia" << endl;
-    cout << "- La fuerza de " << getNombre() << " aumento a " << getFuerza() << endl;
+        mensaje += "\n- " + getNombre() + " activo la habilidad Furia\n";
+        mensaje += "- La fuerza de " + getNombre() + " aumento a " + to_string(getFuerza()) + "\n";
     }
+    return mensaje;
 }
