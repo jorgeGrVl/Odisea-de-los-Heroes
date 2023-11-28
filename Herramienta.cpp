b0310d5767a5dc5bf5cac71ddb28dc4699734841
Jorge Garzón <150094617+jorgeGrVl@users.noreply.github.com>
2023-11-28 14:41:07 -0600

    Final

--- Herramienta.cpp
+++ Herramienta.cpp
@@ -3,27 +3,46 @@
 
 using namespace std;
 
+//Constructor predeterminado de la clase Herramienta. Crea objetos con valores default.
 Herramienta::Herramienta(){
 }
 
+//Constructor parametrizado de la clase Herramienta. Crea objetos con valores definidos.
 Herramienta::Herramienta(string _nombre, string _efecto, int _modificador){
     nombre = _nombre;
     efecto = _efecto;
     modificador = _modificador;
 }
 
-string Herramienta::getNombre(){
+//Regresa una cadena de texto con la información de los atributos de la Herramienta.
+//El método está marcado como const, indicando que no modifica el estado del objeto.
+//Esto es esencial cuando se llama esta función en instancias de Herramienta constantes.
+string Herramienta::getInfo() const{
+    string mensaje = "";
+    mensaje += "- Nombre: " + nombre;
+    mensaje += "\n- Efecto: " + efecto;
+    mensaje += "\n- Cantidad Efecto: " + to_string(modificador) + "\n";
+    return mensaje;
+}
+
+//Regresa el nombre de la Herramienta.
+//El método está marcado como const, indicando que no modifica el estado del objeto.
+//Esto es esencial cuando se llama esta función en instancias de Herramienta constantes.
+string Herramienta::getNombre() const{
     return nombre;
 }
 
+//Regresa el tipo de efecto que tiene la Herramienta.
 string Herramienta::getEfecto(){
     return efecto;
 }
 
+//Regresa la cantidad de modificador de efecto que provee la Herramienta.
 int Herramienta::getModificador(){
     return modificador;
 }
 
+//Regresa un mensaje descriptivo sobre el efecto que aplica la Herramienta.
 string Herramienta::aplicarEfecto(){
     return "- La herramienta " + nombre + " aplico el efecto " + efecto + " sobre ";
 }
